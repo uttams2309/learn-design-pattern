8f0e4211bbea367ad435e9e510edcee44bd60910
uttams2309 <uttams2309@gmail.com>
2025-09-15 10:30:37 +0530

    design patttern learn first commit

---
+++ Dependency_Inversion_Principle/test.java
@@ -0,0 +1,14 @@
+/*
+High level modules should not depend on low level modules.
+Both should depend on abstractions.
+Abstraction should not depend on implementations.
+implementation should depend on abstraction.
+*/
+
+
+
+class test{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
